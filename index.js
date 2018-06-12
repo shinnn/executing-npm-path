b19b269b620cf3c072887de3809b87302c0e7b17
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-12 19:42:56 +0900

    replace `var` with `const`
    [BREAKING] drop support for Node.js < 4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var join = require('path').join;
+const join = require('path').join;
 
-var path = process.env.npm_execpath;
+const path = process.env.npm_execpath;
 
 if (typeof path === 'string' && path.indexOf(join('node_modules', 'npm')) !== -1) {
 	module.exports = path;
